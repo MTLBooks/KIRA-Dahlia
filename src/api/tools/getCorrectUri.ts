55c1f9aeb14d591f6e9a8bdaa807e9edac17fc93
cfdxkk <b2567240058@gmail.com>
2025-03-25 23:03:21 +0800

    refactor(api): return empty string if backend uri is `none`

--- src/api/tools/getCorrectUri.ts
+++ src/api/tools/getCorrectUri.ts
@@ -10,16 +10,22 @@ export default function getCorrectUri(): string {
 
 	try {
 		const backendUriInput = import.meta.env.VITE_BACKEND_URI;
+
 		if (!backendUriInput) {
 			console.error("ERROR", "Server startup failed,  the value of the environment variable VITE_BACKEND_URI was not specified.");
 			return "";
 		}
+
+		if (backendUriInput === "none")
+			return "";
+
 		const backendUri = new URL(backendUriInput.trim());
 		const backendUriHref = backendUri.href;
 		if (!backendUriHref) {
 			console.error("ERROR", "System startup failed, the parsed result of the environment variable VITE_BACKEND_URI is empty.");
 			return "";
 		}
+
 		return backendUriHref;
 	} catch (error) {
 		console.error("ERROR", "System startup failed, environment variable VITE_BACKEND_URI parsing failed: ", error);
