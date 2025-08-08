6239711a1e6e20547675d6f4a60b86b43be1e4d0
Rantetsu Inori <shuishenme@outlook.com>
2025-08-09 02:11:10 +0800

    fix: commit sha slice

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -6,8 +6,8 @@
 		else if (BACKEND_URI.includes("kirakira")) items.push("Live");
 		else if (BACKEND_URI === "none") items.push("No Backend");
 		if (import.meta.env.DEV) items.push("Dev");
-		if (BRANCH) items.push(BRANCH.slice(0, 7));
-		if (COMMIT_SHA) items.push(COMMIT_SHA);
+		if (BRANCH) items.push(BRANCH);
+		if (COMMIT_SHA) items.push(COMMIT_SHA.slice(0, 7));
 		return items.join(" ");
 	});
 </script>
